308817234d17fa146b12be0fa7b6120445e574dd
Paul <paul.bedrossian92@gmail.com>
2026-04-09 08:08:43 +0200

    feat: :sparkles: ajout des méthodes de gestion de la batterie dans Nanosatellite et Node

--- Simu/impl/NanoSatellite.py
+++ Simu/impl/NanoSatellite.py
@@ -9,9 +9,16 @@ class NanosSatellite(Node):
         self.position = position
         self.receivedMessages = []
         self.queuedMessages = []
+        self.battery_level = 100.0
 
     def get_id(self) -> int:
         return self.id
+
+    def get_battery_level(self) -> float:
+        return self.battery_level
+    
+    def consume_battery(self, amount : float) -> None:
+        self.battery_level = max(0.0, self.battery_level - amount)
 
     def get_position(self) -> Position:
         return self.position
--- Simu/interfaces/Node.py
+++ Simu/interfaces/Node.py
@@ -13,6 +13,23 @@ class Node(ABC):
         pass
 
     @abstractmethod
+    def get_battery_level(self) -> float:
+        """
+        Returns the current battery level of the node as a percentage (0-100%).
+        """
+        pass
+
+    @abstractmethod
+    def consume_battery(self, amount : float) -> None:
+        """
+        Consumes a certain amount of battery from the node.
+
+        Arguments:
+        amount - float representing the percentage of battery to consume (e.g., 10.0 for 10%)
+        """
+        pass
+
+    @abstractmethod
     def get_position(self) -> tuple:
         """
         Returns the position of the node as a tuple (x, y, z).
