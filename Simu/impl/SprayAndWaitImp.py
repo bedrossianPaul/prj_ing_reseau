361efba6876972db3d4e8d2a01cc689898cc2981
BONHEUR-ELDJIMBAYE <mbaitodjimreoubonheur@gmail.com>
2026-05-06 17:44:48 +0200

    spray and wait algo

--- Simu/impl/SprayAndWaitImp.py
+++ Simu/impl/SprayAndWaitImp.py
@@ -13,7 +13,8 @@ class SprayAndWaitImp(Router):
         - Phase Spray : si un bundle a encore plus d'une copie, on peut diffuser à tous les nœuds en portée.
         - Phase Wait : si tous les bundles sont à leur dernière copie, on n'envoie pas aux nœuds non destinataires.
         """
-        if any(copies > 1 for copies in self.bundles.values()):
+        if self.L > 1:
+            self.L -= 1  # Réduire le nombre de copies restantes pour chaque bundle
             return nodeInRange
         return []
 
@@ -23,3 +24,15 @@ class SprayAndWaitImp(Router):
             f"SprayAndWaitImp(router_type='SprayAndWait', "
             f"bundles={bundle_count}, L={self.L})"
         )
+    def routing_with_dest(self, nodeInRange: list[Node], dest: Node) -> list[Node]:
+        """
+        Version de routing qui prend en compte la destination.
+        - Phase Spray : si un bundle a encore plus d'une copie, on peut diffuser à tous les nœuds en portée.
+        - Phase Wait : si tous les bundles sont à leur dernière copie, on n'envoie pas aux nœuds non destinataires.
+        """
+        if self.L > 1:
+            self.L -= 1  # Réduire le nombre de copies restantes pour chaque bundle
+            return nodeInRange
+        else:
+            self.L -= 1  # il ne restera plus de copies comme on a déjà croisé le destinataire
+            return [node for node in nodeInRange if node.get_id() == dest.get_id()]
