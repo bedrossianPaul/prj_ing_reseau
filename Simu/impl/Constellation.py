748d41c5ec7c9d5b393eb73da97c7866e6938c77
mohamed22007 <149290083+mohamed22007@users.noreply.github.com>
2026-05-19 14:26:21 +0200

    Ajouter des attack et des pannes

--- Simu/impl/Constellation.py
+++ Simu/impl/Constellation.py
@@ -9,13 +9,19 @@ class Constellation(Network):
         self.nodes = nodes
         self.time = 0
         self.positionsArray = positions
+        self.pannes = set() 
         
 
-    def tick(self) -> None:
+    def tick(self,mode : str, prob: float, range_dist: float) -> None:
         self.time += 1
         for node in self.nodes:
             id = node.get_id()
             node.set_position(self.positionsArray[id][self.time])
+            if self.time % 500 == 0:
+                self.attack_pannes(10, range_dist)
+                
+
+
         return
 
     def get_nodes_in_range(self, node : Node, range : float) -> list[Node]:
@@ -25,15 +31,49 @@ class Constellation(Network):
                 if distance(node.get_position(), otherNode.get_position()) <= range:
                     nodesInRange.append(otherNode)
         return nodesInRange
-    
-    def get_nodes_in_ranges_2(self, node : Node, range : float , prob : float) -> (list[Node] | int):
-        nodesInRange = []
+
+
+    def get_nodes_in_ranges_2(self, node: Node, range_dist: float) -> tuple[list[Node], int]:
+        nodes_active = []
         count_disabled = 0
-        for otherNode in self.nodes:
-            if otherNode.get_id() != node.get_id():
-                if distance(node.get_position(), otherNode.get_position()) <= range:
-                    if (random.random() > prob):  # Simulate probabilistic communication success
-                        nodesInRange.append(otherNode)
-                    else:
+        
+        for other_node in self.nodes:
+            if other_node.get_id() != node.get_id():
+                dist = distance(node.get_position(), other_node.get_position())
+                
+                # Si le nœud est à portée 
+                if dist <= range_dist:
+                    if other_node in self.pannes:
                         count_disabled += 1
-        return (nodesInRange, count_disabled)
+                    else:
+                        nodes_active.append(other_node)
+                        
+        return nodes_active, count_disabled
+    
+
+    def random_pannes(self, prob: float):
+        for node in self.nodes:
+            if node not in self.pannes and random.random() < prob:
+                self.pannes.add(node)
+
+    def gettaille(self, nodes :list[Node] , range : float) -> list[int]:
+        tailles = []
+        for i in nodes :
+            j = 0
+            for node in nodes :
+                if distance(node.get_position(), i.get_position()) <= range:
+                    j = j+ 1
+            tailles.append(j)
+        return tailles
+
+    def attack_pannes(self, n_kill: int, range_dist: float):
+        tailles = self.gettaille(self.nodes, range_dist)
+        for i in range(n_kill):
+            val_max = max(tailles) # Ne pas utiliser "max" comme nom de variable
+            indice = tailles.index(val_max)
+            # CORRECTION : utiliser self.nodes
+            self.pannes.append(self.nodes[indice])
+            tailles[indice] = 0
+        
+        
+            
